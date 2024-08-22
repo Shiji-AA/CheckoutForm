dc80aa903815e970699753ad5551a4098d27db01
shijiaa <shijihcl@gmail.com>
2024-08-22 18:06:50 +0530

    changes added

--- Backend/server.js
+++ Backend/server.js
@@ -3,16 +3,19 @@ import dotenv from "dotenv"
 dotenv.config()
 import router from './routes.js'
 import { connectDB } from './db.js'
-import cors from 'cors';
+import cors from "cors";
 connectDB();
 const app= express();
-app.use(cors({
-    origin: 'https://checkout-form-tjc8-pdga96eo4.vercel.app/', 
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: true 
-  }));
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+    origin: 'https://checkout-form-tjc8-pdga96eo4.vercel.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true 
+  })); 
+
 app.use("/api/users",router);
 
 const port = process.env.PORT || 3001;
