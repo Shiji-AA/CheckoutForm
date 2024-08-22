fbbb2e34a17910f428229c3a0c5d4aa548104ae1
shijiaa <shijihcl@gmail.com>
2024-08-22 17:49:26 +0530

    changes applied

--- Backend/server.js
+++ Backend/server.js
@@ -4,14 +4,13 @@ dotenv.config()
 import router from './routes.js'
 import { connectDB } from './db.js'
 import cors from 'cors';
-
-
 connectDB();
-
-
 const app= express();
-
-app.use(cors());
+app.use(cors({
+    origin: 'https://checkout-form-tjc8-pdga96eo4.vercel.app/', 
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true 
+  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use("/api/users",router);
--- Frontend/src/api/axiosInstance.jsx
+++ Frontend/src/api/axiosInstance.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 const axiosInstance = axios.create({
-    // baseURL: "http://localhost:3000/api/users",
-    baseURL:"https://checkoutform.onrender.com/api/users"
+  //baseURL: "http://localhost:3000/api/users",
+  baseURL: "https://checkoutform.onrender.com/api/users"
+
  
   });
   export default axiosInstance
